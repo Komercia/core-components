17f977d23c6be27e6b5500f8e211c7356f3dc245
Diego Aguilar <daafaguilar98@gmail.com>
2018-04-10 13:51:04 -0500

    components module output

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import Header from './components/headers/header1'
+import Cart1 from './components/carts/cart1'
+import Chat from './components/chat/chat'
+import Header1 from './components/headers/header1'
+import Header2 from './components/headers/header2'
+import Header3 from './components/headers/header3'
+import Product1 from './components/products/product1'
+import Contact1 from './components/contacts/contact1'
 import Slider from './components/sliders/slider1'
 import Separator from './components/separators/separator1'
 import Grid from './components/grids/grid1'
@@ -6,7 +12,13 @@ import Newsletter from './components/newsletters/newsletter1'
 import Footer from './components/footers/footer1'
 
 const components = [
-  Header,
+  Chat,
+  Header1,
+  Header2,
+  Header3,
+  Cart1,
+  Product1,
+  Contact1,
   Slider,
   Separator,
   Grid,
@@ -16,8 +28,8 @@ const components = [
 
 const modules = {}
 modules.install = (Vue) => {
-  for (const component of components) {
-    Vue.component(component.name, component)
+  for (let i = 0, size = components.length; i < size; i += 1) {
+    Vue.component(components[i].name, components[i]);
   }
 }
 
