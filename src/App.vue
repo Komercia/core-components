866edde1a6f09f40c83813682889c19e999f802f
Diego Aguilar <daafaguilar98@gmail.com>
2018-04-10 13:50:01 -0500

    components testing in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,42 @@
 <template>
   <div id="app">
-    <ko-header1 />
-    <ko-slider1 />
-    <ko-separator1 />
-    <ko-grid1 />
-    <ko-newsletter1 />
-    <ko-footer1 />
+    <ko-slider-2/>
   </div>
 </template>
 
 <script>
+import koChat from './components/chat/chat';
+import koContact1 from './components/contacts/contact1';
+import koOrder from './components/_order/order1';
 import koHeader1 from './components/headers/header1';
+import koHeader2 from './components/headers/header2';
+import koHeader3 from './components/headers/header3';
 import koSlider1 from './components/sliders/slider1';
+import koSlider2 from './components/sliders/slider2';
 import koSeparator1 from './components/separators/separator1';
 import koGrid1 from './components/grids/grid1';
 import koNewsletter1 from './components/newsletters/newsletter1';
 import koFooter1 from './components/footers/footer1';
+import koProduct1 from './components/products/product1';
+import cart1 from './components/carts/cart1.vue';
 
 export default {
   name: 'app',
   components: {
+    koContact1,
+    koChat,
+    koOrder,
     koHeader1,
+    koHeader2,
+    koHeader3,
     koSlider1,
+    koSlider2,
     koSeparator1,
     koGrid1,
     koNewsletter1,
     koFooter1,
+    koProduct1,
+    cart1
   },
   created() {
     this.$store.commit('GET_DATA')
@@ -35,4 +46,14 @@ export default {
 
 <style>
 @import './assets/css/main.css';
+:root {
+  --main_color: #00acc1;
+  --text_color: #fff;
+  --button_color: #00acc1;
+  --button_text_color: #fff;
+  --background_color: #FFF;
+}
+#app{
+  background-color: var(--background_color);
+}
 </style>
