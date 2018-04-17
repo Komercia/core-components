1b718a726b5264f5e5896693ec765439d4dd794e
Diego Aguilar <daafaguilar98@gmail.com>
2018-04-17 11:30:11 -0500

    Add Icons

---
+++ src/Icons/AlignJustify.vue
@@ -0,0 +1,3 @@
+<template>
+  <path d="M17 2v1h-17v-1h17zM0 7h17v-1h-17v1zM0 11h17v-1h-17v1zM0 15h17v-1h-17v1z" fill="#000000" />
+</template>
---
+++ src/Icons/Text.vue
@@ -0,0 +1,3 @@
+<template>
+  <path d="M14 2v3h-1v-2h-4v12h1.643v1h-4.286v-1h1.643v-12h-4v2h-1v-3h11z"/>
+</template>
