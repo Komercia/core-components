7c84c091277ad02d45957fa4def6e92490130cbe
Diego Aguilar <daafaguilar98@gmail.com>
2018-04-18 18:37:55 -0500

    update file of export modules

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import Header2 from './components/headers/header2'
 import Header3 from './components/headers/header3'
 import Product1 from './components/products/product1'
 import Contact1 from './components/contacts/contact1'
-import Slider from './components/sliders/slider1'
+import Slider1 from './components/sliders/slider1'
+import Slider2 from './components/sliders/slider2'
 import Separator from './components/separators/separator1'
 import Grid from './components/grids/grid1'
+import ProductList1 from './components/product_lists/product_list1'
 import Newsletter from './components/newsletters/newsletter1'
 import Footer from './components/footers/footer1'
+
+import SliderSetting1 from './settings/sliders/slider1'
+import SeparatorSetting1 from './settings/separators/separator1'
 
 const components = [
   Chat,
@@ -19,11 +24,15 @@ const components = [
   Cart1,
   Product1,
   Contact1,
-  Slider,
+  Slider1,
+  Slider2,
   Separator,
   Grid,
+  ProductList1,
   Newsletter,
-  Footer
+  Footer,
+  SliderSetting1,
+  SeparatorSetting1
 ]
 
 const modules = {}
@@ -32,6 +41,5 @@ modules.install = (Vue) => {
     Vue.component(components[i].name, components[i]);
   }
 }
-
 
 export default modules
