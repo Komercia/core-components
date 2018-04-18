d145f9936d9bacce054e3584fffbce7fa1612bb4
Diego Aguilar <daafaguilar98@gmail.com>
2018-04-18 18:36:07 -0500

    Create product_list

---
+++ src/components/product_lists/product_list1.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="product_list_wrapper">
+    <paginate
+      name="products"
+      :list="products"
+      :per="12"
+      tag="div"
+      class="product_list">
+      <ko-product-card v-for="product in paginated('products')" :key="product.id" :data="product" />
+    </paginate>
+    <paginate-links
+      for="products"
+      :show-step-links="true"
+      :limit="5">
+    </paginate-links>
+  </div>
+</template>
+
+<script>
+import KoProductCard from '../_productCard/product-card';
+export default {
+  name: 'KoProductList1',
+  components: { KoProductCard },
+  data() {
+    return {
+      paginate: ['products'],
+    }
+  },
+  computed: {
+    products() {
+      return this.$store.state.productsData;
+    },
+  },
+}
+</script>
+
+<style>
+  .product_list_wrapper{
+    width: 100%;
+    display: grid;
+    grid-template-columns: minmax(auto, 1200px);
+    justify-content: center;
+    justify-items: center;
+    margin: 20px 0;
+  }
+  .product_list {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-auto-rows: auto;
+    grid-gap: 100px;
+    margin: 0 auto;
+    padding: 20px 0;
+  }
+  .paginate-links.products {
+    display: flex;
+    background-color: #FFF;
+    margin-top: 30px;
+    /*border: 1px solid black;*/
+  }
+  .paginate-links.products li{
+    width: 35px;
+    height: 35px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: var(--text_color);
+    cursor: pointer;
+  }
+  /*.paginate-links.products li ~ li{
+    border-left: 1px solid black;
+  }*/
+  .paginate-links.products li.active{
+    background-color: var(--button_color);
+    color: var(--button_text_color);
+  }
+  .paginate-links.products li[class$="arrow"] {
+    /*background-color: var(--button_color);*/
+    color: var(--button_color);
+    font-weight: bold;
+    font-size: 30px;
+    /*border-radius: 50%;*/
+  }
+</style>
