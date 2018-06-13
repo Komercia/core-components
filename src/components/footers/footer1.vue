f10bd46258dfc3da1a10f2f42d186425166eb534
Diego Aguilar <daafaguilar98@gmail.com>
2018-06-13 09:17:39 -0500

    fix routes Footer

--- src/components/footers/footer1.vue
+++ src/components/footers/footer1.vue
@@ -1,14 +1,14 @@
 <template>
   <footer class="footer">
     <div class="footer-container-icons">
-      <a v-show="info.red_instagram" :href="info.red_instagram" class="footer-icon"><i class="icon icon-instagrem"></i></a>
-      <a v-show="info.red_twitter" :href="info.red_twitter" class="footer-icon"><i class="icon icon-twitter" ></i></a>
-      <a v-show="info.red_facebook" :href="info.red_facebook" class="footer-icon"><i class="icon icon-facebook-square" ></i></a>
-      <a v-show="info.red_youtube" :href="info.red_youtube" class="footer-icon"><i class="icon icon-youtube"></i></a>
+      <a v-show="info.red_instagram" :href="info.red_instagram" target="_blank" class="footer-icon"><i class="icon icon-instagrem"></i></a>
+      <a v-show="info.red_twitter" :href="info.red_twitter" target="_blank" class="footer-icon"><i class="icon icon-twitter" ></i></a>
+      <a v-show="info.red_facebook" :href="info.red_facebook" target="_blank" class="footer-icon"><i class="icon icon-facebook-square" ></i></a>
+      <a v-show="info.red_youtube" :href="info.red_youtube" target="_blank" class="footer-icon"><i class="icon icon-youtube"></i></a>
     </div>
     <nav class="container-menu-footer-list">
       <ul class="menu-footer-list">
-        <li v-for='(item, index) in routesFooter' :key='index' class="menu-footer"><a class="menu-footer-link" href="#">{{item.name}}</a></li>
+        <li v-for='(item, index) in routesFooter' :key='index' class="menu-footer"><router-link class="menu-footer-link" :to="item.route">{{item.name}}</router-link></li>
       </ul>
     </nav>
   </footer>
@@ -21,16 +21,16 @@ export default {
     return {
       routesFooter: [
         {
-          name: 'Contacto',
-          route: '/contacto',
+          name: 'Sobre nosotros',
+          route: '/nosotros',
         },
         {
-          name: 'Politicas',
-          route: '/politicas',
+          name: 'Politicas de garantia',
+          route: '/garantia',
         },
         {
-          name: 'Productos',
-          route: '/productos',
+          name: 'Manejo de datos',
+          route: '/manejo-de-datos',
         },
       ]
     }
