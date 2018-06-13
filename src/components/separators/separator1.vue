4ee5e047ca295f22048040d17d60711058697da3
Diego Aguilar <daafaguilar98@gmail.com>
2018-06-13 09:18:12 -0500

    fix separator

--- src/components/separators/separator1.vue
+++ src/components/separators/separator1.vue
@@ -1,33 +1,39 @@
 <template>
   <div class="container-separator">
-    <h2 class="title-separator" v-show="setting.title">{{ setting.title }}</h2>
-    <p class="subtitle-separator" v-show="setting.description">{{ setting.description }}</p>
+    <h2 class="title-separator" v-if="setting.title">{{ setting.title }}</h2>
+    <p class="subtitle-separator" v-if="setting.description">{{ setting.description }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'koSeparator1',
-  props: ['setting'],
-  computed: {
-    info() {
-      return this.$store.state.tienda;
-    },
-  },
+  props: {
+    setting: {
+      type: Object,
+      default: function () {
+        return { title: 'Titulo',  description: 'Descripción del separador' }
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
 .container-separator {
+  max-width: 1366px;
+  width: 96%;
+  min-height: 60px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 60px;
+  margin: 0 auto;
   padding: 40px 0;
 }
 .title-separator {
   font-weight: bold;
   font-size: 20px;
+  color: var(--text_color);
 }
 .subtitle-separator {
   padding-top: 8px;
