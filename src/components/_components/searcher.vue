fafb2b50f9564b83074b560fd5d4b2d369aa6ed2
rusbelllozano <kiloloco2@gmail.com>
2018-07-04 09:06:26 -0500

    Change name of method sendsearch

--- src/components/_components/searcher.vue
+++ src/components/_components/searcher.vue
@@ -1,7 +1,7 @@
 <template lang="html">
 <div  class="searcher" >
-  <el-input placeholder="Buscar producto" v-model="search" class="input-with-select" @keydown.enter.prevent="Sendsearch">
-    <el-button slot="append" icon="el-icon-search"button @click="Sendsearch" > </el-button>
+  <el-input placeholder="Buscar producto" v-model="search" class="input-with-select" @keydown.enter.prevent="sendSearch">
+    <el-button slot="append" icon="el-icon-search"button @click="sendSearch" > </el-button>
   </el-input>
 </div>
 </template>
@@ -11,21 +11,14 @@ export default {
   name:'koSearcher',
   data() {
     return {
-      input: '',
       search:'',
     }
   },
   methods:{
-    Sendsearch(){
+    sendSearch(){
       this.$emit("searcher",this.search)
     },
-
   },
-  computed:{
-    products() {
-      return this.$store.state.productsData;
-    },
-  }
 }
 </script>
 
