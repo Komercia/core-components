b02401f70ee88708d615c5592ee109cff05b204b
gusplayer <fosterstudios.colombia@gmail.com>
2018-08-01 18:58:12 -0500

    create a settings panel, with button for hidden/show and organize the code

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,96 @@
   <template>
-  <div id="app">
-    <!-- <koChat /> -->
-    <!-- <cart1 /> -->
-    <!-- <koHeader1 /> -->
-    <!-- <koHeader2 /> -->
-    <!-- <koContact1 /> -->
-    <!-- <koFooter1 /> -->
-    <!-- <koFooter2 /> -->
-    <!-- <koGrid1 /> -->
-    <!-- <koHeader3 /> -->
-    <!-- <koNewsletter1 /> -->
-    <!-- <koContent1/> -->
-    <!-- <ProductList1/> -->
-    <!-- <ProductList2/> -->
-    <!-- <koProduct1 /> -->
-    <!-- <koSeparator1 /> -->
-    <!-- <koSlider1 /> -->
-    <!-- <Slider11 /> -->
-    <!-- <koSlider2/> -->
+  <div id="app" class="container">
+    <div :class="{container_settings: true, hidden: showSettingsButton}">
+      <div class="title">
+        <h1>Settings</h1>
+      </div>
+
+      <div class="component_setting">
+        <div :is="selectComponent + 'Settings' " />
+      </div>
+
+      <div class="container_responsive_icons ">
+        <img src="./assets/compu.png" width="50px ">
+        <img src="./assets/tablet.png" width="50px ">
+        <img src="./assets/phone.png" width="50px ">
+      </div>
+    </div>
+
+    <div class="container_components ">
+      <div class="title " style="margin-bottom: 10px ">
+        <a v-on:click="hiddenSettings" class="hidden_settings_button">
+          <el-button type="primary" icon="el-icon-menu" circle></el-button>
+          Mostrar Settings
+        </a>
+        <h1>Componente Principal
+          <el-select v-model="selectComponent" placeholder="Select Component " style="margin-left:10px ">
+            <el-option v-for="item in components " :key="item " :label="item " :value="item "></el-option>
+          </el-select>
+        </h1>
+
+        <h1>Arriba
+          <i class="el-icon-upload2" />
+          <el-select v-model="selectComponentAbove " placeholder="Select Component " style="margin-left:10px ">
+            <el-option v-for="item2 in components " :key="item2 " :label="item2 " :value="item2 "></el-option>
+          </el-select>
+        </h1>
+
+        <h1>Abajo
+          <i class="el-icon-download" />
+          <el-select v-model="selectComponentDown " placeholder="Select Component " style="margin-left:10px ">
+            <el-option v-for="item in components " :key="item " :label="item " :value="item "></el-option>
+          </el-select>
+        </h1>
+      </div>
+
+      <div class="component_principal">
+        <div :is=selectComponentAbove />
+        <div :is=selectComponent />
+        <div :is=selectComponentDown />
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import koContent1 from './components/contents/content1';
-import koChat from './components/chat/chat';
-import koOrder1 from './components/_order/order1';
+import koContent1 from "./components/contents/content1";
+import koChat from "./components/chat/chat";
+import koOrder1 from "./components/_order/order1";
 
-import cart1 from './components/carts/cart1.vue';
+import cart1 from "./components/carts/cart1.vue";
 
-import koContact1 from './components/contacts/contact1';
+import koContact1 from "./components/contacts/contact1";
 
+import koFooter1 from "./components/footers/footer1";
+import koFooter2 from "./components/footers/footer2";
 
-import koFooter1 from './components/footers/footer1';
-import koFooter2 from './components/footers/footer2';
+import koGrid1 from "./components/grids/grid1";
 
-import koGrid1 from './components/grids/grid1';
+import koHeader1 from "./components/headers/header1";
+import koHeader2 from "./components/headers/header2";
+import koHeader3 from "./components/headers/header3";
 
-import koHeader1 from './components/headers/header1';
-import koHeader2 from './components/headers/header2';
-import koHeader3 from './components/headers/header3';
+import koNewsletter1 from "./components/newsletters/newsletter1";
 
-import koNewsletter1 from './components/newsletters/newsletter1';
+import ProductList1 from "./components/product_lists/product_list1";
+import ProductList2 from "./components/product_lists/product_list2";
 
-import ProductList1 from './components/product_lists/product_list1';
-import ProductList2 from './components/product_lists/product_list2';
+import koProduct1 from "./components/products/product1";
 
-import koProduct1 from './components/products/product1';
+import koSeparator1 from "./components/separators/separator1";
 
-import koSeparator1 from './components/separators/separator1';
+import koSlider1 from "./components/sliders/slider1";
+import Slider11 from "./components/sliders/slider1-1";
+import koSlider2 from "./components/sliders/slider2";
 
-import koSlider1 from './components/sliders/slider1';
-import Slider11 from './components/sliders/slider1-1';
-import koSlider2 from './components/sliders/slider2';
+// Settings components /////
+
+import koSlider2Settings from "./settings/slider2";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     koChat,
     koOrder1,
@@ -74,49 +110,185 @@ export default {
     koSlider1,
     Slider11,
     koSlider2,
-    koContent1
-
+    koContent1,
+    //Settings////////////
+    koSlider2Settings
   },
   created() {
-    this.$store.commit('GET_DATA')
+    this.$store.commit("GET_DATA");
   },
   data() {
     return {
-      settingBanner: {
-        banners: [
-          {
-            position: 'center',
-          },
-          {
-            position: 'center',
-          }
-        ]
-      }
+      selectComponent: "koSlider2",
+      showSettingsButton: true,
+      selectComponentAbove: "",
+      selectComponentDown: "",
+      components: [
+        "koChat",
+        "koOrder1",
+        "cart1",
+        "koContact1",
+        "koFooter1",
+        "koFooter2",
+        "koGrid1",
+        "koHeader1",
+        "koHeader2",
+        "koHeader3",
+        "koNewsletter1",
+        "ProductList1",
+        "ProductList2",
+        "koProduct1",
+        "koSeparator1",
+        "koSlider1",
+        "Slider11",
+        "koSlider2",
+        "koContent1"
+      ],
+      options3: [
+        {
+          label: "Popular cities",
+          options: [
+            {
+              value: "Shanghai",
+              label: "Shanghai"
+            },
+            {
+              value: "Beijing",
+              label: "Beijing"
+            }
+          ]
+        },
+        {
+          label: "City name",
+          options: [
+            {
+              value: "Chengdu",
+              label: "Chengdu"
+            },
+            {
+              value: "Shenzhen",
+              label: "Shenzhen"
+            },
+            {
+              value: "Guangzhou",
+              label: "Guangzhou"
+            },
+            {
+              value: "Dalian",
+              label: "Dalian"
+            }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {
+    hiddenSettings() {
+      this.showSettingsButton == true
+        ? (this.showSettingsButton = false)
+        : (this.showSettingsButton = true);
     }
   }
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Open+Sans');
-@import './assets/css/main.css';
+@import url("https://fonts.googleapis.com/css?family=Open+Sans");
+@import "./assets/css/main.css";
+@import "./styles/material.css";
+@import "./styles/flat.css";
 
-*{
-  font-family: 'Open Sans', sans-serif;
+* {
+  font-family: "Open Sans", sans-serif;
 }
 
 :root {
-  --main_color: #F200FF;
-  --text_color: #292B32;
-  --button_color: #F200FF;
-  --button_text_color: #FFF;
-  --background_color: #FFF;
+  --main_color: #f200ff;
+  --text_color: #292b32;
+  --button_color: #f200ff;
+  --button_text_color: #fff;
+  --background_color: #fff;
 }
 
-#app{
+#app {
   background-color: var(--background_color);
 }
-.el-slider__button-wrapper{
+.el-slider__button-wrapper {
   z-index: 98 !important;
+}
+.container {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100vh;
+  background-color: #e8ecef;
+}
+.container_settings {
+  display: grid;
+  width: 450px;
+  height: 100%;
+  padding: 0 20px;
+  background-color: #e8ecef;
+}
+.component_setting {
+  margin-top: 10px;
+  width: 100%;
+  overflow: auto;
+}
+.container_responsive_icons {
+  display: flex;
+  width: 100%;
+  height: 70px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  background: #fff;
+  border-radius: 2px;
+  justify-content: space-around;
+  align-items: center;
+  align-self: flex-end;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+.container_responsive_icons img {
+  border-radius: 50px;
+  cursor: pointer;
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+.container_responsive_icons img:hover {
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.25), 0 5px 5px rgba(0, 0, 0, 0.22);
+  transform: scale(1.1);
+}
+.hidden_settings_button {
+  cursor: pointer;
+  font-size: 14px;
+  text-decoration: underline;
+}
+.container_components {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #e8ecef;
+}
+.component_principal {
+  overflow: auto;
+  height: 100%;
+  width: 100%;
+}
+.title {
+  display: flex;
+  width: 100%;
+  height: 60px;
+  margin-top: 0px;
+  align-items: center;
+  justify-content: space-around;
+  background-color: white;
+}
+.title h1 {
+  font-size: 15px;
+  font-weight: 800;
+  color: #4c4c4c;
+}
+.hidden {
+  display: none;
 }
 </style>
---
+++ src/settings/separator1.vue
@@ -0,0 +1,172 @@
+<template>
+  <section class="sectionBar">
+    <header class="sectionBar_header">
+      <span class="angle-left" @click="closeSetting">
+        <!-- <icon-base icon-name="angle-left">
+          <angle-left />
+        </icon-base> -->
+      </span>
+      <p v-if="settingData">{{ settingData.label }}</p>
+      <span></span>
+    </header>
+    <section class="settingBanner">
+      <div class="input-area">
+        <el-input placeholder="Titulo" v-model="settingData.title">
+          <template slot="prepend">
+            <!-- <icon-base icon-name="text">
+              <icon-text /></icon-base> -->
+          </template>
+        </el-input>
+      </div>
+      <div class="input-area">
+        <el-input placeholder="Descripción" v-model="settingData.description">
+          <template slot="prepend">
+            <!-- <icon-base icon-name="align-justify">
+              <align-justify />
+            </icon-base> -->
+          </template>
+        </el-input>
+      </div>
+      <div class="settingBanner_actions">
+        <!-- <el-button @click="updateBanner(banner)">Guardar</el-button> -->
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+// import AngleLeft from "../../Icons/AngleLeft.vue";
+// import IconText from "../../Icons/Text.vue";
+// import AlignJustify from "../../Icons/AlignJustify.vue";
+// import IconLinks from "../../Icons/Links.vue";
+
+export default {
+  name: "koSeparator1Settings",
+  // components: { AngleLeft, IconText, AlignJustify, IconLinks },
+  computed: {
+    settingData() {
+      return this.$store.state.settingData;
+    }
+  },
+  methods: {
+    closeSetting() {
+      this.$store.state.settingData = null;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.sectionBar {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  background-color: #e9ecef;
+  z-index: 3;
+}
+.sectionBar_header {
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  background-color: #fff;
+  box-sizing: border-box;
+  box-shadow: 0 0 0 1px rgba(63, 63, 68, 0.05),
+    0 1px 3px 0 rgba(63, 63, 68, 0.15);
+}
+.sectionBar_header p {
+}
+.sectionBar_header span {
+  flex: 1;
+  display: flex;
+  align-items: center;
+}
+.sectionBar_header span.angle-left {
+  cursor: pointer;
+}
+.form_list {
+  height: calc(100vh - 184px);
+  box-sizing: border-box;
+  padding: 10px 5px;
+  overflow: auto;
+}
+.settingBanner {
+  padding: 20px 0;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+.input-area {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 5px 0;
+}
+.input-area p {
+  margin-bottom: 5px;
+  font-size: 14px;
+  color: #333;
+}
+.upload {
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border-radius: 2px;
+  box-shadow: 1px 1px 4px 0 rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+}
+.upload .hover {
+  position: absolute;
+  opacity: 0;
+  border-color: #818592;
+  background-color: rgba(255, 255, 255, 0.5);
+  transition: 0.3s;
+}
+.upload .hover i {
+  color: #818592;
+}
+.upload .hover:hover {
+  opacity: 1;
+}
+.upload img {
+  max-width: 100%;
+  max-height: 100%;
+}
+.upload div {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px dashed #d8dce5;
+  padding: 10px 0;
+}
+.upload div i {
+  font-size: 35px;
+  color: #b1b5bc;
+}
+#uploadBanner {
+  visibility: hidden;
+  opacity: 0;
+  width: 0;
+  height: 0;
+  display: none;
+}
+.newBanner_action {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: #f5f7fa;
+  box-sizing: border-box;
+  border: 1px solid #dcdfe6;
+  border-radius: 4px;
+  padding: 5px;
+  color: #909399;
+  margin-top: 10px;
+  font-size: 14px;
+}
+</style>
