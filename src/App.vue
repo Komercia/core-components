790b10770895dc467131de0dbee0956064ada6f5
Diego Aguilar <daafaguilar98@gmail.com>
2018-08-02 17:15:19 -0500

    remove imports of components

--- src/App.vue
+++ src/App.vue
@@ -44,9 +44,9 @@
       </div>
 
       <div class="component_principal">
-        <div :is=selectComponentAbove />
-        <div :is=selectComponent />
-        <div :is=selectComponentDown />
+        <div :is="selectComponentAbove" />
+        <div :is="selectComponent" />
+        <div :is="selectComponentDown" />
       </div>
 
     </div>
@@ -55,66 +55,11 @@
 </template>
 
 <script>
-import koContent1 from "./components/contents/content1";
-import koChat from "./components/chat/chat";
-import koOrder1 from "./components/_order/order1";
-
-import cart1 from "./components/carts/cart1.vue";
-
-import koContact1 from "./components/contacts/contact1";
-
-import koFooter1 from "./components/footers/footer1";
-import koFooter2 from "./components/footers/footer2";
-
-import koGrid1 from "./components/grids/grid1";
-
-import koHeader1 from "./components/headers/header1";
-import koHeader2 from "./components/headers/header2";
-import koHeader3 from "./components/headers/header3";
-
-import koNewsletter1 from "./components/newsletters/newsletter1";
-
-import ProductList1 from "./components/product_lists/product_list1";
-import ProductList2 from "./components/product_lists/product_list2";
-
-import koProduct1 from "./components/products/product1";
-
-import koSeparator1 from "./components/separators/separator1";
-
-import koSlider1 from "./components/sliders/slider1";
-import Slider11 from "./components/sliders/slider1-1";
-import koSlider2 from "./components/sliders/slider2";
-
-// Settings components /////
-
-import koSlider2Settings from "./settings/slider2";
 
 export default {
   name: "app",
-  components: {
-    koChat,
-    koOrder1,
-    cart1,
-    koContact1,
-    koFooter1,
-    koFooter2,
-    koGrid1,
-    koHeader1,
-    koHeader2,
-    koHeader3,
-    koNewsletter1,
-    ProductList1,
-    ProductList2,
-    koProduct1,
-    koSeparator1,
-    koSlider1,
-    Slider11,
-    koSlider2,
-    koContent1,
-    //Settings////////////
-    koSlider2Settings
-  },
   created() {
+    this.$store.dispatch('GET_COMPONENTS')
     this.$store.commit("GET_DATA");
   },
   data() {
@@ -123,27 +68,6 @@ export default {
       showSettingsButton: true,
       selectComponentAbove: "",
       selectComponentDown: "",
-      components: [
-        "koChat",
-        "koOrder1",
-        "cart1",
-        "koContact1",
-        "koFooter1",
-        "koFooter2",
-        "koGrid1",
-        "koHeader1",
-        "koHeader2",
-        "koHeader3",
-        "koNewsletter1",
-        "ProductList1",
-        "ProductList2",
-        "koProduct1",
-        "koSeparator1",
-        "koSlider1",
-        "Slider11",
-        "koSlider2",
-        "koContent1"
-      ],
       options3: [
         {
           label: "Popular cities",
@@ -181,6 +105,11 @@ export default {
         }
       ]
     };
+  },
+  computed: {
+    components () {
+      return this.$store.state.components
+    }
   },
   methods: {
     hiddenSettings() {
