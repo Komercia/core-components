fa55a07906d63cf551e69f296d44d196e8f7a707
Diego Aguilar <daafaguilar98@gmail.com>
2018-08-02 17:11:28 -0500

    firebase connect file

---
+++ src/utils/connect_firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCCQZKHS6vcparUI-0pLJDWJmU8YKzVszY",
+  authDomain: "komercia-2c50b.firebaseapp.com",
+  databaseURL: "https://komercia-2c50b.firebaseio.com",
+  projectId: "komercia-2c50b",
+  storageBucket: "komercia-2c50b.appspot.com",
+  messagingSenderId: "1098181302565"
+});
+
+export default firebase
