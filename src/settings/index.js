5ab9f7596df49ab2a1ada95d7dac1bafa9cf6dcb
Diego Aguilar <daafaguilar98@gmail.com>
2018-08-08 17:22:42 -0500

    change name import of video

--- src/settings/index.js
+++ src/settings/index.js
@@ -2,14 +2,14 @@ import SliderSetting1 from './Sliders/slider1'
 import SliderSetting11 from './Sliders/slider1-1'
 import SeparatorSetting1 from './Separators/separator1'
 import ContentSetting1 from './Contents/content1'
-import video1 from './Videos/video1'
+import VideoSetting1 from './Videos/video1'
 
 const components = [
   SliderSetting1,
   SliderSetting11,
   SeparatorSetting1,
   ContentSetting1,
-  video1
+  VideoSetting1
 ]
 
 export default components
