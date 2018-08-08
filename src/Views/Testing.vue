a662fcb4d97875ced0e0c231408eb3990c8e826f
DESKTOP-VKKL5LV\usuario <titanmar15@gmail.com>
2018-08-08 12:02:18 -0500

    se agregó fuente de iconos

--- src/Views/Testing.vue
+++ src/Views/Testing.vue
@@ -10,7 +10,7 @@
 <script>
 import Menu from '../components/headers/header4'
 import koSlider3 from '../components/sliders/slider3'
-import Grid2 from '../components/grids/grid-categorias-home'
+import Grid2 from '../components/grids/grid-categories'
 import Footer3 from '../components/footers/footer3'
 export default {
   components: { Menu, Grid2, koSlider3, Footer3 }
