7b60c09d3e7a7e33cc6b0c30f2db7e6773f6aa4d
Diego Aguilar <daafaguilar98@gmail.com>
2018-08-09 15:55:11 -0500

    rename

--- src/Views/Testing.vue
+++ src/Views/Testing.vue
@@ -10,7 +10,7 @@
 <script>
 import Menu from '../components/headers/header4'
 import koSlider3 from '../components/sliders/slider3'
-import Grid2 from '../components/grids/grid-categories'
+import Grid2 from '../components/grids/grid2'
 import Footer3 from '../components/footers/footer3'
 export default {
   components: { Menu, Grid2, koSlider3, Footer3 }
