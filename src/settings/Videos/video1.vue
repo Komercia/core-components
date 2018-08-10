9b5e52e0868e1d6719caf301b7ca4ee293873d97
Diego Aguilar <daafaguilar98@gmail.com>
2018-08-10 14:34:16 -0500

    build video1 setting

--- src/settings/Videos/video1.vue
+++ src/settings/Videos/video1.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="setting">
-    <div class="row">
-      <div class="">
-        <p class="title">Destino:</p>
-        <p class="desc">Escoge de donde es la url de tu video</p>
-      </div>
-      <el-radio-group v-model="settingData.type" size="mini">
-        <el-radio-button label="Youtube"></el-radio-button>
-        <el-radio-button label="Vimeo"></el-radio-button>
-      </el-radio-group>
+    <div class="">
+      <p class="title">Destino del video</p>
+      <p class="desc">Escoge el origen del video y luego escribe la url en el campo de texto</p>
     </div>
+    <el-radio-group v-model="settingData.type">
+      <el-radio-button label="Youtube"></el-radio-button>
+      <el-radio-button label="Vimeo"></el-radio-button>
+    </el-radio-group>
     <el-input placeholder="Escribe la url" v-model="settingData.url">
       <template slot="prepend">
         <icon-base
@@ -32,6 +30,11 @@ import IconVimeo from "../../Icons/Vimeo.vue";
 export default {
   name: 'koVideoSetting1',
   components: { IconYoutube, IconVimeo },
+  watch: {
+    'settingData.type': function () {
+      this.settingData.url = ''
+    }
+  },
   computed: {
     settingData () {
       return this.$store.state.settingData
@@ -41,15 +44,31 @@ export default {
 </script>
 
 <style scoped>
+  .setting{
+    display: grid;
+    justify-content: center;
+    justify-items: center;
+    grid-row-gap: 7px;
+    padding: 10px;
+  }
   .row{
     display: grid;
     grid-auto-flow: column;
     margin-bottom: 10px;
   }
   .title {
-    font-weight: 600;
+    text-align: center;
+    font-size: 30px;
+    font-weight: 300;
+    background-color: transparent;
+    margin-bottom: 10px;
   }
   .desc {
-    font-size: 13px;
+    box-sizing: border-box;
+    padding: 0 20px;
+    line-height: 1.5;
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.568);
+    text-align: center;
   }
 </style>
