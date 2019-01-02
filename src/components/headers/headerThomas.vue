b130c5aafd6886a08a55f42a4a46bfec5b61d13f
DESKTOP-VKKL5LV\usuario <titanmar15@gmail.com>
2019-01-02 16:56:18 -0500

    se creo componente koHeaderThomas

---
+++ src/components/headers/headerThomas.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-header">
+    <div class="container">
+      <div class="content-header"></div>
+      <div class="logo">Logo</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "koHeader5"
+};
+</script>
+
+<style scoped>
+.main-header {
+  width: 100%;
+  height: 80px;
+  background-color: blueviolet;
+}
+</style>
