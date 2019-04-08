ca5dfccff846011072184c3c2502b9f9f4fbaa44
Diego Aguilar <daafaguilar98@gmail.com>
2019-04-08 16:44:21 -0500

    header4

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,17 +1,17 @@
-import SliderSetting1 from './Sliders/slider1'
-import SliderSetting11 from './Sliders/slider1-1'
-import SliderSetting1v3 from './Sliders/slider1/v3'
-import SliderSetting3 from './Sliders/slider3'
-import SliderSetting4 from './Sliders/slider4/v1.vue'
-import SliderSetting5 from './Sliders/slider5/v1.vue'
-import SeparatorSetting1 from './Separators/separator1'
-import ContentSetting1 from './Contents/content1'
-import koContent2Setting from './Contents/content2/v1'
-import VideoSetting1 from './Videos/video1'
-import GridSetting2 from './Grids/grid2'
-import HeaderSetting1 from './Headers/thomas/v1'
-import HeaderSetting2 from './Headers/header6/v1'
-import HeaderSetting4 from './Headers/header4/v1'
+import SliderSetting1 from './Sliders/slider1';
+import SliderSetting11 from './Sliders/slider1-1';
+import SliderSetting1v3 from './Sliders/slider1/v3';
+import SliderSetting3 from './Sliders/slider3';
+import SliderSetting4 from './Sliders/slider4/v1.vue';
+import SliderSetting5 from './Sliders/slider5/v1.vue';
+import SeparatorSetting1 from './Separators/separator1';
+import ContentSetting1 from './Contents/content1';
+import koContent2Setting from './Contents/content2/v1';
+import VideoSetting1 from './Videos/video1';
+import GridSetting2 from './Grids/grid2';
+import HeaderSetting1 from './Headers/thomas/v1';
+import HeaderSetting2 from './Headers/header6/v1';
+import HeaderSetting4v1 from './Headers/header4/v1';
 
 const components = [
   SliderSetting1,
@@ -27,7 +27,7 @@ const components = [
   GridSetting2,
   HeaderSetting1,
   HeaderSetting2,
-  HeaderSetting4
-]
+  HeaderSetting4v1
+];
 
-export default components
+export default components;
