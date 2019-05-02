9d7e2741a15a3334ec2a5e849797727713445cd9
Diego Aguilar <daafaguilar98@gmail.com>
2019-05-02 13:47:44 -0500

    improve App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,47 @@
   <div id="app" :class="`container ${selectStyle}`">
     <!-- <button @click="handle_style('defaultstyle')">Default</button>
     <button @click="handle_style('materialstyle')">Material Design</button>
-    <button @click="handle_style('borderstyle')">Borderless</button> -->
-    <router-view />
+    <button @click="handle_style('borderstyle')">Borderless</button>-->
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: "app",
   created() {
-    this.$store.dispatch('GET_COMPONENTS')
-    this.$store.dispatch('GET_SECTIONS')
-    this.$store.commit('GET_DATA')
+    this.$store.dispatch("GET_COMPONENTS");
+    this.$store.dispatch("GET_SECTIONS");
+    this.$store.commit("GET_DATA");
   },
   data() {
     return {
-      selectStyle: ''
-    }
+      selectStyle: ""
+    };
   },
   methods: {
     handle_style(style) {
-      this.selectStyle = style
+      this.selectStyle = style;
     }
   }
-}
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
-@import './assets/css/main.css';
-@import './styles/material.css';
-@import './styles/default.css';
-@import './styles/borderless.css';
+@import url("https://fonts.googleapis.com/css?family=Open+Sans:400,600,700");
+@import "./assets/css/main.css";
+@import "./styles/material.css";
+@import "./styles/default.css";
+@import "./styles/borderless.css";
 * {
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
 }
 :root {
   --main_color: #333;
-  --text_color: #292b32;
+  --text_color: #fff;
   --button_color: #333;
   --button_text_color: #fff;
-  --background_color: #fff;
+  --background_color: #333;
 }
 #app {
   background-color: var(--background_color);
